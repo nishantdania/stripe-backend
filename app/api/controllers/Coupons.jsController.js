8837c17e461c95e07a630f036d7d4f7025b83ce6
nishantdania <nishantdania@gmail.com>
2017-01-20 22:04:14 +0530

    added coupons api

---
+++ app/api/controllers/Coupons.jsController.js
@@ -0,0 +1,11 @@
+/**
+ * Coupons.jsController
+ *
+ * @description :: Server-side logic for managing Coupons.js
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
