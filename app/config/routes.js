43e50a0d0d9a4cd0a757e29eaa1b4a3b1948b6df
nishantdania <nishantdania@gmail.com>
2017-01-20 21:35:16 +0530

    added user model

--- app/config/routes.js
+++ app/config/routes.js
@@ -34,7 +34,9 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  'POST /signup': 'UserController.create'
+  
 
   /***************************************************************************
   *                                                                          *
